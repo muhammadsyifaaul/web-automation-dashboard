ccf3573e487e2d6fc4b6b807d9b623bd34039c20
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2025-12-06 19:19:03 +0700

    feat: Initialize React application entry point with App component and environment variable logging.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,5 +6,8 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
+        
     </React.StrictMode>,
 )
+console.log('API_URL:', import.meta.env.VITE_API_URL);
+console.log('ENV:', import.meta.env.VITE_ENV);
