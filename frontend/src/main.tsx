63cc3b9e9b66f7077f1aa11997bb9eed5bb90ba5
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2025-12-06 20:07:57 +0700

    feat: initialize React application entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,5 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         
     </React.StrictMode>,
 )
-console.log('API_URL:', import.meta.env.VITE_API_URL);
-console.log('ENV:', import.meta.env.VITE_ENV);
+
